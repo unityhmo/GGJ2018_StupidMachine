c42cf76b34dd84da83542547695905d61c696fe1
TonyMtz <atony.mtz@gmail.com>
2018-01-27 14:30:36 -0700

    improved gears and pivots

--- Unity Project/StupidMachine/Assets/Scripts/Gear.cs
+++ Unity Project/StupidMachine/Assets/Scripts/Gear.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Gear : MonoBehaviour
 {
     private GearController _gearController;
-    private GameObject _target;
-    private bool _isMouseDrag;
-    private Vector3 _screenPosition;
-    private Vector3 _offset;
+
 
     private void Start()
     {
@@ -34,71 +31,6 @@ public class Gear : MonoBehaviour
             {
                 _gearController.CancelSelectedPivot(pivot);
             }
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hitInfo;
-            _target = ReturnClickedObject(out hitInfo);
-            if (_target != null)
-            {
-                _isMouseDrag = true;
-                Debug.Log("target position :" + _target.transform.position);
-                //Convert world position to screen position.
-                _screenPosition = Camera.main.WorldToScreenPoint(_target.transform.position);
-                _offset = _target.transform.position -
-                          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
-                              _screenPosition.z));
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            _isMouseDrag = false;
-            SetPositionOnSelectedPivot();
-        }
-
-        if (_isMouseDrag)
-        {
-            //track mouse position.
-            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPosition.z);
-
-            //convert screen position to world position with offset changes.
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + _offset;
-
-            //It will update target gameobject's current postion.
-            _target.transform.position = currentPosition;
-        }
-    }
-
-    private GameObject ReturnClickedObject(out RaycastHit hit)
-    {
-        GameObject target = null;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
-        {
-            target = hit.collider.gameObject;
-        }
-
-        return target;
-    }
-
-    private void SetPositionOnSelectedPivot()
-    {
-        GameObject selectedPivot = _gearController.SelectedPivot;
-
-        if (selectedPivot != null)
-        {
-            Vector3 newPosition = new Vector3(
-                selectedPivot.transform.position.x,
-                selectedPivot.transform.position.y,
-                transform.position.z
-            );
-            transform.position = newPosition;
         }
     }
 }
--- Unity Project/StupidMachine/Assets/Scripts/GearController.cs
+++ Unity Project/StupidMachine/Assets/Scripts/GearController.cs
@@ -4,17 +4,89 @@ using UnityEngine;
 
 public class GearController : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _pivots;
+    [SerializeField] private List<GameObject> _pivots;
     [SerializeField] GameObject _selectedPivot;
 
-    // Use this for initialization
-    void Start()
+    private GameObject _target;
+    private bool _isMouseDrag;
+    private Vector3 _screenPosition;
+    private Vector3 _offset;
+    private GearController _gearController;
+
+    private void Start()
     {
+        _gearController = GameObject.FindGameObjectWithTag("GearController").GetComponent<GearController>();
+
+        _pivots = new List<GameObject>();
+
+        foreach (GameObject pivot in GameObject.FindGameObjectsWithTag("Pivot"))
+        {
+            _pivots.Add(pivot);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            _target = ReturnClickedObject(out hitInfo);
+            if (_target != null && _target.layer == 9) // Gears
+            {
+                _isMouseDrag = true;
+                //Convert world position to screen position.
+                _screenPosition = Camera.main.WorldToScreenPoint(_target.transform.position);
+                _offset = _target.transform.position -
+                          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                              _screenPosition.z));
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isMouseDrag = false;
+            SetPositionOnSelectedPivot();
+        }
+
+        if (_isMouseDrag)
+        {
+            //track mouse position.
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPosition.z);
+
+            //convert screen position to world position with offset changes.
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + _offset;
+
+            //It will update target gameobject's current postion.
+            _target.transform.position = currentPosition;
+        }
+    }
+
+    private GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+
+        return target;
+    }
+
+    private void SetPositionOnSelectedPivot()
+    {
+        GameObject selectedPivot = _gearController.SelectedPivot;
+
+        if (selectedPivot != null)
+        {
+            Vector3 newPosition = new Vector3(
+                selectedPivot.transform.position.x,
+                selectedPivot.transform.position.y,
+                transform.position.z
+            );
+            _target.transform.position = newPosition;
+        }
     }
 
     public void SetSelectedPivot(Pivot pivot)
