892bff63363597673babc1bd6adda8421383cd2b
TonyMtz <atony.mtz@gmail.com>
2018-01-28 14:58:19 -0700

    audio

---
+++ Unity Project/StupidMachine/Assets/Audio/ButtonSounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSounds : MonoBehaviour
+{
+    [SerializeField] private AudioClip _buttonClick;
+
+    private AudioManager _audioManager;
+
+    private void Start()
+    {
+        _audioManager = AudioManager.Instance;
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(PlayGearClickSound);
+    }
+
+    public void PlayGearClickSound()
+    {
+        _audioManager.PlaySound(_buttonClick);
+    }
+}
